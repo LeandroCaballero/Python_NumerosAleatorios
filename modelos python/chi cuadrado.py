28156d773b00dbff5742a9498b5475450e119653
LeandroCaballero <leandrorojo248@hotmail.com>
2019-05-22 11:00:50 -0300

    actualizacion

---
+++ modelos python/chi cuadrado.py
@@ -0,0 +1,10 @@
+ #n=55; k=10; n pi = 55/10 = 5.5; gl = 9.
+ #n=cantidad de numeros
+ #k=numeros posibles
+ #fi=cantidad de veces q aparece el numero en la sucesion 
+ #gl = k-1
+
+ #operacion:
+ #sumatoria ((fi-n pi)**2)/n pi
+ #comparar ese resultado con el numero de la tabla, si es menor se acepa hipotesis
+ #http://labrad.fisica.edu.uy/docs/tabla_chi_cuadrado.pdf
--- modelos python/intervalos.py
+++ modelos python/intervalos.py
@@ -1,27 +1,46 @@
 #xi+1 = (a*xi)%m
 
-print("Comienzo del intervalo:")
-a = int(input())
+#print("Comienzo del intervalo:")
+#a = int(input())
+#
+#print("Final del intervalo:")
+#b = int(input())
+#
+#print("Semilla:")
+#semilla = int(input())
+#
+#print("Constante Multiplicativa:")
+#const = int(input())
+#
+#print("Modulo:")
+#m = int(input())
+#
+#print("Numeros requeridos:")
+#iteraciones = int(input())
+#n = 0
+#
+#while n < iteraciones:
+#    numero = (const*semilla) % m 
+#    r1 = (numero*(b-a))+a
+#    print(r1)
+#    semilla = numero
+#    n = n+1
 
-print("Final del intervalo:")
-b = int(input())
-
+#---------------------------------------------------------------------
 print("Semilla:")
 semilla = int(input())
+k=int(input("Numero mayor a 2: "))
+min=int(input("Ingrese valor minimo: "))
+max=int(input("Ingrese valor maximo: "))
+num=int(input("Ingrese cantidad de valores deseados: "))
 
-print("Constante Multiplicativa:")
-const = int(input())
+c=10*k+1
+m=max
 
-print("Modulo:")
-m = int(input())
+i=0
+while i<num:
+    semilla=(c*semilla)%m
 
-print("Numeros requeridos:")
-iteraciones = int(input())
-n = 0
-
-while n < iteraciones:
-    numero = (const*semilla) % m 
-    r1 = (numero*(b-a))+a
-    print(r1)
-    semilla = numero
-    n = n+1
+    if semilla>min:
+        print(semilla)
+        i=i+1
